3e4610bf72a407ea75a7ae9c1f9c4c3f6db8b268
Rafael Henrique Madeira Feitosa <rafael.henrique.mad@gmail.com>
2022-11-23 16:53:19 -0300

    Feed
    Creating Feed

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Feed from "./Feed/Feed";
 
 const Home = () => {
   return (
-    <div style={{ border: "1px solid black", height: "80vh" }}>
-      <h1>Home</h1>
-    </div>
+    <section className="container mainContainer">
+      <Feed />
+    </section>
   );
 };
 
