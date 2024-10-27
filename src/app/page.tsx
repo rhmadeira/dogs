ce9d9b6f35e69cd1ef8c7ab1d540ceec7937969e
Rafael Henrique Madeira Feitosa <rafael.henrique.mad@gmail.com>
2024-10-27 14:56:26 -0300

    Refactor layout.tsx to improve component structure and add container classes Refactor .gitignore file Add ErrorMessage component Add apiError function Add styles to feed-modal.module.css Update Home component to fetch and display photos Add loading styles to loading.module.css Add FeedModal component Add photo-get action Add FeedPhotos component Add photos-get action Add styles to feed.module.css Add api functions Add Feed component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
-export default function Home() {
+import photosGet from "@/actions/photos-get";
+import Feed from "@/components/feed/feed";
+
+export default async function Home() {
+  const { data } = await photosGet();
+  console.log(data);
   return (
     <section className="container mainContainer">
       <h1 className="title">Home</h1>
+      <Feed photos={data ?? []} />
     </section>
   );
 }
