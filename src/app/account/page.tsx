c661c00e31201565405f0c72588e292dffd0bc89
Rafael Henrique Madeira Feitosa <rafael.henrique.mad@gmail.com>
2025-01-04 18:33:53 -0300

    feat: implement user context and authentication flow with token validation and logout functionality

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,7 +1,31 @@
+import photosGet from "@/actions/photos-get";
+import userGet from "@/actions/user-get";
+import Feed from "@/components/feed/feed";
+import Link from "next/link";
+
 export default async function AccountPage() {
+  const user = await userGet();
+  const photos = await photosGet({ user: user.data?.username });
   return (
-    <main>
-      <h1>Account</h1>
-    </main>
+    <section>
+      {photos.data?.length ? (
+        <Feed photos={photos.data} user={user.data?.username} />
+      ) : (
+        <div>
+          <p
+            style={{ color: "#444", fontSize: "1.25rem", marginBottom: "1rem" }}
+          >
+            Nenhuma foto encontrada.
+          </p>
+          <Link
+            href={"/conta/postar"}
+            className="button"
+            style={{ display: "inline-block" }}
+          >
+            Postar Foto
+          </Link>
+        </div>
+      )}
+    </section>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,25 +3,31 @@ import "./globals.css";
 import { type_second } from "@/functions/fonts";
 import Header from "@/components/layout/header";
 import Footer from "@/components/layout/footer";
+import { UserContextProvider } from "@/context/user-context";
+import userGet from "@/actions/user-get";
 
 export const metadata: Metadata = {
   title: "Dogs",
   description: "Dogs are the best, social media for dogs",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const { data: user } = await userGet();
+
   return (
     <html lang="en">
       <body className={type_second.variable}>
-        <div className="App">
-          <Header />
-          <main className="AppBody">{children}</main>
-          <Footer />
-        </div>
+        <UserContextProvider user={user}>
+          <div className="App">
+            <Header />
+            <main className="AppBody">{children}</main>
+            <Footer />
+          </div>
+        </UserContextProvider>
       </body>
     </html>
   );
